e308b09791e3721c0557dae705eebf20ef9af0eb
Michael <michaelleojacob@gmail.com>
2022-02-22 20:28:33 -0500

    got my first redux state working!

--- src/app/components/counter.js
+++ src/app/components/counter.js
@@ -1,7 +1,16 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './counterSlice';
+
 const Counter = () => {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div>
       <div>hello from counter</div>
+      {count}
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+import store from './app/store/store';
 import Nav from './app/components/nav';
 import App from './app/app';
 import Counter from './app/components/counter';
@@ -9,12 +11,14 @@ import Counter from './app/components/counter';
 const rootElement = document.getElementById('root');
 render(
   <BrowserRouter>
-    <Nav />
-    <div>=========================================================</div>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/counter" element={<Counter />} />
-    </Routes>
+    <Provider store={store}>
+      <Nav />
+      <div>=========================================================</div>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </Provider>
   </BrowserRouter>,
   rootElement,
 );
